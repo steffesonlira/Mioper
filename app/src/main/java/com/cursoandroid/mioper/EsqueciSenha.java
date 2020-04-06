81057410d8ea61ba42ffadc9a009b8b561082ec3
Denis <dennis.jnt@gmail.com>
2020-04-06 13:45:11 -0300

    Agora exibe informação se não caso não tiver nenhuma viagem realizada no histórico

--- app/src/main/java/com/cursoandroid/mioper/EsqueciSenha.java
+++ app/src/main/java/com/cursoandroid/mioper/EsqueciSenha.java
@@ -1,6 +1,7 @@
 package com.cursoandroid.mioper;
 
 //region Imports
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,11 +12,15 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 //endregion
 
 public class EsqueciSenha extends AppCompatActivity {
@@ -25,6 +30,7 @@ public class EsqueciSenha extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private Button buttonEsqueciSenha;
     private EditText userEmail;
+
     //endregion
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,21 +51,21 @@ public class EsqueciSenha extends AppCompatActivity {
         buttonEsqueciSenha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(email.getText().toString().equals("")){
+                if (email.getText().toString().equals("")) {
                     Toast.makeText(EsqueciSenha.this, "Favor inserir o e-mail para redefinição de senha"
                             , Toast.LENGTH_SHORT).show();
                     return;
                 }
-                // region Enviar ao e-mail cadastrado no Firebase Auth um link com o formulário de recuperação de senha
+                //Enviar ao e-mail cadastrado no Firebase Auth um link com o formulário de recuperação de senha
                 firebaseAuth
                         .sendPasswordResetEmail(email.getText().toString())
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
-                                if(task.isSuccessful()){
+                                if (task.isSuccessful()) {
                                     final ProgressDialog progressDialog = new ProgressDialog(EsqueciSenha.this);
                                     progressDialog.setIndeterminate(true);
-                                    progressDialog.setMessage("Enviando e-mail para recuperar senha...");
+                                    progressDialog.setMessage("Enviando e-mail para recuperação de senha...");
                                     progressDialog.show();
 
                                     new android.os.Handler().postDelayed(
@@ -69,17 +75,21 @@ public class EsqueciSenha extends AppCompatActivity {
                                                 }
                                             }, 2500);
                                     email.setText("");
-                                    Toast.makeText(EsqueciSenha.this,
-                                            "Recuperação de acesso iniciado, e-mail enviado!",
-                                            Toast.LENGTH_SHORT)
-                                            .show();
 
-                                }else{
 
+                                } else {
+                                    String excecao = "";
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthInvalidUserException e) {
+                                        excecao = "Usuário não está cadastrado.";
+                                    } catch (Exception e) {
+                                        excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                                        e.printStackTrace();
+                                    }
                                     Toast.makeText(EsqueciSenha.this,
-                                            "Falhou, tente novamente",
-                                            Toast.LENGTH_SHORT)
-                                            .show();
+                                            excecao,
+                                            Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
@@ -91,7 +101,7 @@ public class EsqueciSenha extends AppCompatActivity {
 
     //Criar requisição de Senha esquecida
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
     }
 
@@ -110,7 +120,8 @@ public class EsqueciSenha extends AppCompatActivity {
                 startActivity(new Intent(this, Login.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
                 finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
                 break;
-            default:break;
+            default:
+                break;
         }
         return true;
     }
